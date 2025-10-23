269a3275ba76374402920fa92f78bd4489353cef
amir <amiralifakhari9@gmail.com>
2025-10-23 23:05:02 +0330

    upd members

--- app.js
+++ app.js
@@ -171,14 +171,14 @@ function renderEvent(event, piName, piShortName) {
  */
 function renderPI(pi) {
     const scholarIcon = `<svg fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24"><path d="M12 24a7 7 0 1 1 0-14 7 7 0 0 1 0 14zm0-24L0 9.5l4.838 3.39L12 18l7.162-5.11L24 9.5z"/></svg>`;
-    const githubIcon = `<svg fill="currentColor" class="w-5 h-5" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.ss-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>`;
+    const githubIcon = `<svg fill="currentColor" class="w-5 h-5" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>`;
 
     return `
     <div class="bg-white dark:bg-gray-900 rounded-lg shadow-xl overflow-hidden md:flex border border-gray-200 dark:border-gray-700">
         <div class="md:flex-shrink-0">
-            <img class="h-12 w-full  md:h-12 md:w-12" src="${pi.imageUrl}" alt="${pi.name}" onerror="this.src='https://placehold.co/400x400/93C5FD/1F2937?text=PI+Photo';">
+            <img class="w-full h-48 object-cover md:w-64 md:h-auto md:aspect-square" src="${pi.imageUrl}" alt="${pi.name}" onerror="this.src='https://placehold.co/400x400/93C5FD/1F2937?text=PI+Photo';">
         </div>
-        <div class="p-8">
+        <div class="p-8 flex-grow">
             <div class="flex justify-between items-start">
                 <div>
                     <h3 class="text-2xl font-bold text-gray-900 dark:text-white">${pi.name}</h3>
@@ -208,13 +208,8 @@ function renderTeamMember(member) {
     const hasDetails = member.bio || member.scholarUrl || member.githubUrl;
 
     return `
-    <div class="group relative w-fit text-center bg-white dark:bg-gray-900 rounded-lg shadow-lg border border-gray-200 dark:border-gray-700 overflow-hidden">
-        <img 
-        class="w-[17rem] h-[17rem] object-cover mx-auto rounded"
-        src="${member.imageUrl}"
-        alt="${member.name}"
-        onerror="this.src='https://placehold.co/300x300/E5E7EB/374151?text=Photo';"
-        />
+    <div class="group relative text-center bg-white dark:bg-gray-900 rounded-lg shadow-lg border border-gray-200 dark:border-gray-700 overflow-hidden">
+        <img class="w-full aspect-square object-cover" src="${member.imageUrl}" alt="${member.name}" onerror="this.src='https://placehold.co/300x300/E5E7EB/374151?text=Photo';">
         <div class="p-4">
             <h4 class="text-lg font-semibold text-gray-900 dark:text-white">${member.name}</h4>
             <p class="text-gray-500 dark:text-gray-400">${member.title}</p>
